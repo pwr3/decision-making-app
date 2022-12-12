bf95469b67c49cb189e8487e7733e34adebb5395
Viktor <pwr3dnn@gmail.com>
2022-12-13 00:49:51 +0100

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import React from 'react';
-// import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import React from "react";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Layout from "./view/Layout";
 
-// const router = createBrowserRouter([
-//     {
-//         path: '/la',
-//         element: <>asdasdasd</>,
-//         errorElement: <>Not Found......</>,
-//         children: [
-//             {
-//                 path: 'issues/',
-//                 element: <>Option Page</>
-//             }
-//         ]
-//     },
-//     {
-//         path: '/fu',
-//         element: <>FUUUU</>
-//     }
-// ])
-
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        // errorElement: <>Not Found......</>,
+        children: [
+            {
+                path: 'issues/:id',
+                element: <>Option Page</>
+            }
+        ]
+    },
+    {
+        path: '/fu',
+        element: <>FUUUU</>
+    }
+])
 const App = () => {
-  return (
-      // <RouterProvider router={router}></RouterProvider>
-    <>cccccccc</>
-  );
+    return(
+        <RouterProvider router={router}></RouterProvider>
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/index';
-import App from './App';
-
-
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <StrictMode>
       <Provider store={store}>
-        <App />
+          <App />
       </Provider>
-    </StrictMode>
 );
