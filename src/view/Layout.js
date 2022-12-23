3a282c8d1a4cd2c5f95f8cac1d6d852ce15e535b
Viktor <pwr3dnn@gmail.com>
2022-12-23 17:22:04 +0100

    panel expand feature added

--- src/view/Layout.js
+++ src/view/Layout.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Stack, Box } from "@mui/material";
 import IssuesPage from "./IssuesPage";
 
 const Layout = () => {
+    const location = useLocation();
+    console.log(location)
     return (
         <div>
+            {(location.pathname === '/') ? <Stack
+                sx={{ mx: 6 }}
+                direction="row"
+                justifyContent="center"
+                alignItems="flex-start"
+            >
+                <Box sx={{ width: "55%", px: 2 }}>
+                    <IssuesPage />
+                </Box>
+            </Stack> :
             <Stack
                 sx={{ mx: 6 }}
                 direction="row"
@@ -18,7 +30,7 @@ const Layout = () => {
                 <Box sx={{ width: "55%", px: 2 }}>
                     <Outlet />
                 </Box>
-            </Stack>
+            </Stack> }
         </div>
     );
 };
